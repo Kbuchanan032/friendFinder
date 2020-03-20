3190dfcde26716901da5a3b79d6464e0aec0b1a2
Keith Buchanan <Kbuchanan77@yahoo.com>
2020-03-19 17:01:47 -0700

    added final touches. Now to debug

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friends = [
+  {
+    name: "S. Elliot",
+    photo: " ",
+    scores: ["3", "5", "2", "2", "4", "3", "5", "1", "4", "1"]
+  },
+  {
+    name: "RDJ",
+    photo: " ",
+    scores: ["2", "1", "2", "5", "2", "4", "2", "2", "1", "2"]
+  },
+  {
+    name: "This guy",
+    photo: " ",
+    scores: ["5", "5", "5", "2", "3", "1", "5", "4", "4", "2"]
+  },
+  {
+    name: "Brett",
+    photo: " ",
+    scores: ["4", "3", "2", "1", "4", "3", "2", "1", "4", "1"]
+  },
+  {
+    name: "That one guy",
+    photo: "../images/thatOneGuy.webp",
+    scores: ["5", "5", "5", "5", "4", "4", "4", "3", "3", "3"]
+  },
+  {
+    name: "That other guy",
+    photo: "../images/ThatOtherGuy.jpg",
+    scores: ["1", "4", "3", "2", "1", "2", "2", "5", "3", "5"]
+  }
+];
